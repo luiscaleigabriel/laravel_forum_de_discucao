5c4e8cb2acc6d726fda50745c36d4fc246fdb9a0
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-02-28 18:50:00 +0100

    .

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index() {
-        return view('admin.supports.index');
+    public function index(Support $support) {
+
+        $supports = $support->all();
+
+        dd($supports);
+
+        return view('admin.supports.index', compact('supports'));
     }
 }
